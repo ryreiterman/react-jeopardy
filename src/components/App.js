1040d922c14cb4309b1e90bb8c5c195ebb2336d5
Ryan Reiterman <ryreiterman@gmail.com>
2021-01-27 18:34:32 -0500

    Before class commit

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ export default function App(props) {
 	const [category, setCategory] = useState('');
 	const [question, setQuestion] = useState('');
 	const [answer, setAnswer] = useState('');
+	const [toggle, setToggle] = useState(false);
 
 	const getData = async () => {
 		const response = await fetch(`http://jservice.io/api/random`);
@@ -15,17 +16,30 @@ export default function App(props) {
 		setCategory(jeopardyData.category.title.toUpperCase());
 		setQuestion(jeopardyData.question);
 		setAnswer(jeopardyData.answer);
+		setToggle(false);
 
-		console.log(jeopardyData);
+		console.log(data[0]);
+	};
+
+	const toggleAnswer = () => {
+		toggle ? setToggle(false) : setToggle(true);
 	};
 
 	return (
 		<div className="Page-wrapper">
 			<h1>Jeopardy App</h1>
-			<button onClick={getData} value="Get Question" />
-			<h2>{category}</h2>
-			<h2>{question}</h2>
-			<h2>{answer}</h2>
+			<img
+				src="https://innerstrength.zone/wp-content/uploads/2020/03/FI-FUN-2020-03-11_17-51-07-667x354.jpg"
+				alt="Will Ferrell"
+			/>
+			<button onClick={getData}>Get Question</button>
+			<h2>Points: {score}</h2>
+			<h2>Category: {category}</h2>
+			<h2>Question: {question}</h2>
+			<button onClick={toggleAnswer}>Show Answer</button>
+			<div className={toggle ? 'answer' : 'no-answer'}>
+				<h2>Answer: {answer.toUpperCase()}</h2>
+			</div>
 		</div>
 	);
 }
